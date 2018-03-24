7a0456acdddf6c1af9c2d6f459f89c3bacf66bac
Kachel Wittig <kjwittig@gmail.com>
2018-03-24 00:22:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 class Shoe
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
 
   def turn_page
